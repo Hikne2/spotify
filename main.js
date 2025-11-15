c837deced99609de5df5bf9bba9cd1e43e46d902
Hikne <hikneeee2@gmail.com>
2025-11-15 19:30:47 +0100

    update url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import Spotify from "./spotify.js";
 
 const clientId = "a6e9021c90c34cbca9d2cdec3ab272be";
-const redirectUri = "http://localhost:5500"; // change this if needed
+const redirectUri = "https://hikne2.github.io/spotify/"; // change this if needed
 const api = new Spotify(clientId, redirectUri); 
 
 document.getElementById("login").onclick = () => api.login();
